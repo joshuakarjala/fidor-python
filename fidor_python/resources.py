9408049ff3d49806bac9d248cd57038aed8587fd
Joshua Karjala-Svendsen <joshua@founders.as>
2014-11-05 17:53:05 +0100

    basic features implemented

--- fidor_python/resources.py
+++ fidor_python/resources.py
@@ -7,23 +7,20 @@ GET = 1
 POST = 2
 
 
-def baseRequest(endpoint, method, resource_id=None, meta_filter=None, payload={}, page=1):
+def baseRequest(endpoint, method, resource_id=None, meta_filter=None, payload={}, params={'page': 1}):
     headers = {'Content-Type': 'application/json'}
+    params.update({'access_token': fidor_python.access_token})
 
-    url = '{base_url}{endpoint}/{resource_id}/{meta_filter}?access_token={access_token}&page={page}'.format(
+    url = '{base_url}{endpoint}{resource_id}/{meta_filter}'.format(
         base_url=fidor_python.test_api_url,
         endpoint=endpoint,
-        resource_id=resource_id or '',
-        meta_filter=meta_filter or '',
-        access_token=fidor_python.access_token,
-        page=page)
-
-    print url
+        resource_id="/{resource_id}".format(resource_id=resource_id) if resource_id else '',
+        meta_filter="/{meta_filter}".format(meta_filter=meta_filter) if meta_filter else '')
 
     if method == GET:
-        r = requests.get(url, headers=headers)
+        r = requests.get(url, params=params, headers=headers)
     elif method == POST:
-        r = requests.post(url, headers=headers, data=json.dumps(payload))
+        r = requests.post(url, params=params, headers=headers, data=json.dumps(payload))
 
     return r.json()
 
@@ -50,19 +47,75 @@ class Transactions():
 
         return baseRequest('/transactions', GET)
 
-
     @classmethod
     def get(transaction_id):
         return baseRequest('/transactions', GET, transaction_id)
 
 
+class InternalTransfers():
+
+    @classmethod
+    def all(cls, params={'page': 1}, batch=False):
+        if batch:
+            return baseRequest('/batch_transfers', GET, params=params)
+        return baseRequest('/internal_transfers', GET, params=params)
+
+    @classmethod
+    def get(cls, transaction_id, batch=False):
+        if batch:
+            return baseRequest('/batch_transfers', GET, transaction_id)
+        return baseRequest('/internal_transfers', GET, transaction_id)
+
+    @classmethod
+    def create(cls, payload, batch=False):
+        if batch:
+            return baseRequest('/batch_transfers', POST, payload=payload)
+        return baseRequest('/internal_transfers', POST, payload=payload)
+
+
+class SepaCreditTransfers():
+
+    @classmethod
+    def all(cls, params={'page': 1}, batch=False):
+        if batch:
+            return baseRequest('/batch_transfers', GET, params=params)
+        return baseRequest('/sepa_credit_transfers', GET, params=params)
+
+    @classmethod
+    def get(cls, transaction_id, batch=False):
+        if batch:
+            return baseRequest('/batch_transfers', GET, transaction_id)
+        return baseRequest('/sepa_credit_transfers', GET, transaction_id)
+
+    @classmethod
+    def create(cls, payload, batch=False):
+        if batch:
+            return baseRequest('/batch_transfers', POST, payload=payload)
+        return baseRequest('/sepa_credit_transfers', POST, payload=payload)
+
+
+class SepaMandates():
+
+    @classmethod
+    def all(cls, params={'page': 1}):
+        return baseRequest('/sepa_mandates', GET, params=params)
+
+    @classmethod
+    def get(cls, mandate_id):
+        return baseRequest('/sepa_mandates', GET, mandate_id)
+
+    @classmethod
+    def create(cls, payload):
+        return baseRequest('/sepa_mandates', POST, payload=payload)
+
+
 class SepaDirectDebits():
 
     @classmethod
-    def all(cls, page=1, batch=False):
+    def all(cls, params={'page': 1}, batch=False):
         if batch:
-            return baseRequest('/batch_direct_debits', GET, page=page)
-        return baseRequest('/sepa_direct_debits', GET, page=page)
+            return baseRequest('/batch_direct_debits', GET, params=params)
+        return baseRequest('/sepa_direct_debits', GET, params=params)
 
     @classmethod
     def get(cls, transaction_id, batch=False):
@@ -75,18 +128,3 @@ class SepaDirectDebits():
         if batch:
             return baseRequest('/batch_direct_debits', POST, payload=payload)
         return baseRequest('/sepa_direct_debits', POST, payload=payload)
-
-
-class SepaCreditTransfers():
-
-    @classmethod
-    def all(cls, page=1):
-        return baseRequest('/sepa_credit_transfers', GET, page=page)
-
-    @classmethod
-    def get(cls, transaction_id):
-        return baseRequest('/sepa_credit_transfers', GET, transaction_id)
-
-    @classmethod
-    def create(cls, payload):
-        return baseRequest('/sepa_credit_transfers', POST, payload=payload)
